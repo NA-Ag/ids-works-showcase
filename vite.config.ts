cdda24853362a3d7c9e7802542bb6409451b989e
NA-Ag <nathanaguila22@gmail.com>
2026-04-17 13:15:59 -0600

    Github actions fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/ids-works-showcase/',
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
