c186719028d18d8bc0f3e88152fa1adc1aabd166
NA-Ag <nathanaguila22@gmail.com>
2026-04-20 15:19:00 -0600

    general fix

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Section } from './Section';
 import { Button } from './Button';
+import { Link } from 'react-router-dom';
 import {
   Users, Calculator, BookOpen, GraduationCap,
   Library, Coffee, UserCircle, AlertTriangle,
